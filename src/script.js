2a3f137b05e6b1bf8098385da17887722de108b3
Pavan Ingalagi <ingalagipavan@gmail.com>
2023-02-26 17:03:16 +0530

    github

---
+++ src/script.js
@@ -0,0 +1,45 @@
+// Sticky Navbar
+let header = document.querySelector("header");
+let menu = document.querySelector("#menu-icon");
+let navbar = document.querySelector(".navbar");
+let resumedow = document.getElementById("resume-link-2");
+let resume1 = document.getElementById("resume-button-1");
+
+window.addEventListener("scroll", () => {
+  header.classList.toggle("shadow", window.scrollY > 0);
+});
+
+menu.onclick = () => {
+  navbar.classList.toggle("active");
+};
+window.onscroll = () => {
+  navbar.classList.remove("active");
+};
+
+resumedow.onclick = () => {
+    window.open("https://drive.google.com/file/d/1EFyDOQnBuZ8qzpgii6FlJOQq3IgRJ00R/view");
+    setTimeout(() => {
+        window.location.href="https://drive.google.com/u/0/uc?id=1EFyDOQnBuZ8qzpgii6FlJOQq3IgRJ00R&export=download";
+    }, 5);
+};
+
+resume1.onclick = () => {
+    window.open("https://drive.google.com/file/d/1EFyDOQnBuZ8qzpgii6FlJOQq3IgRJ00R/view");
+    setTimeout(() => {
+        window.location.href="https://drive.google.com/u/0/uc?id=1EFyDOQnBuZ8qzpgii6FlJOQq3IgRJ00R&export=download";
+    }, 5);
+
+};
+
+// Dark Mode
+// let darkmode = document.querySelector("#darkmode");
+
+darkmode.onclick = () => {
+  if (darkmode.classList.contains("bx-moon")) {
+    darkmode.classList.replace("bx-moon", "bx-sun");
+    document.body.classList.add("active");
+  } else {
+    darkmode.classList.replace("bx-sun", "bx-moon");
+    document.body.classList.remove("active");
+  }
+};
